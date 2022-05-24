2146e8eeb5886d772f1d99ac77799240d8bbfc8e
vaibhav-gogia <vaibhavgogia01@gmail.com>
2022-05-24 11:04:05 -0400

    Lists

---
+++ Lists.py
@@ -0,0 +1,12 @@
+#Lists 
+from re import M
+
+
+myList = ["banana", "cherry", "apple"]
+print(myList)
+
+myList2 = [5, True, "apple", "apple"]
+print(myList2)
+print(myList2[1])
+#for i in myList:
+ #   print(i)
