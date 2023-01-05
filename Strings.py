3fe6a099e1a70c68dd2c90d1f4af378a6e9884ea
vaibhav-gogia <61176566+vaibhav-gogia@users.noreply.github.com>
2023-01-05 11:54:02 -0500

    Strings over

--- Strings.py
+++ Strings.py
@@ -46,3 +46,59 @@ print(my_string2.lower())
 
 print(my_string2.startswith('   Hello'))
 print(my_string2.find('o'))
+
+new_string = 'how are you doing'
+new_list = new_string.split(" ")
+print(new_list) 
+
+#checking the time of the 2 weys below
+
+hehe_list = ['a']*6
+print(hehe_list)
+from timeit import default_timer as timer
+
+#bad python code
+start = timer()
+hehe_string = ''
+for i in hehe_list:
+    hehe_string += i
+stop = timer()
+print(stop-start)
+print(hehe_string)
+
+#good way
+start = timer()
+hehe2_string = ''.join(hehe_list)
+stop = timer()
+print(stop-start)
+print(hehe2_string)
+
+
+#formatting strings
+# %, .format(), f-Strings
+
+var = 3.1234567
+var_string = "the variable is %f" % var
+print(var_string)
+
+var2_string = "the variable is {}".format(var)
+print(var2_string)
+
+#newest and the best way
+var3_string = f"the variable is {var}"
+print(var3_string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
